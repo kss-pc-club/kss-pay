a41eeda3577530a6b0d5c5e78455394051a012a0
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-12-24 00:18:58 +0900

    Create firebaseConfig.js

---
+++ pre-commit.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# ref: https://qiita.com/ryounagaoka/items/3e7a1b44d43ad0547d4f
+
+unchangeable_files=(
+  ^src/firebaseConfig.js
+)
+
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$1" =~ $e ]] && return 0; done
+  return 1
+}
+
+for FILE in `git diff --cached --name-status $against -- | cut -c3-`; do
+  if containsElement $FILE "${unchangeable_files[@]}"; then
+    echo "$FILE"
+    CHANGE_DETECTED=1
+  fi
+done
+
+if [ "$CHANGE_DETECTED" ]; then
+  echo "Failed to commit because of the modification of the files not permitted."
+  exit 1
+fi
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDL9XGWSENz1_7P3KoFFT3rKHv4aBuQGGs',
+  authDomain: 'kss-pay-f0a57.firebaseapp.com',
+  databaseURL: 'https://kss-pay-f0a57.firebaseio.com',
+  projectId: 'kss-pay-f0a57',
+  storageBucket: 'kss-pay-f0a57.appspot.com',
+  messagingSenderId: '204708930204',
+  appId: '1:204708930204:web:8b7421c5d3f7d32ecf8359',
+}
+
+export { firebaseConfig }
