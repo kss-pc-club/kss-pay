0cf05d6e1bf72fe842971b5d2dcc416b9c976dce
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-02-28 23:53:35 +0900

    Create type.ts: タイプ一覧ファイル

---
+++ src/type.ts
@@ -0,0 +1,43 @@
+//----- 使用するタイプ一覧 -----//
+import { firebase } from './firebase'
+
+// 使用できるプロバイダのクラス。今のところGoogle、Facebook、Twitterの3種。
+type Provider =
+  | firebase.auth.GoogleAuthProvider
+  | firebase.auth.FacebookAuthProvider
+  | firebase.auth.TwitterAuthProvider
+
+// 使用できるプロバイダの名前
+type providers = 'facebook' | 'google' | 'twitter' | 'mail'
+
+// ユーザー認証データ
+type usrAuthData = {
+  name: string | null
+  uid: string | null
+  mail: string | null | undefined
+  google: string | null | undefined
+  twitter: string | null | undefined
+  facebook: string | null | undefined
+  mailVerified: boolean
+}
+
+// モーダルの引数
+type modal_option = {
+  ttl: string
+  detail: string
+  link?: {
+    show: boolean
+    url: string
+    ttl: string
+  }
+}
+
+// ユーザー データベース内のデータ
+type userDBData =
+  | {
+      barcode: string
+      money: number
+    }
+  | undefined
+
+export { Provider, providers, usrAuthData, modal_option, userDBData }
