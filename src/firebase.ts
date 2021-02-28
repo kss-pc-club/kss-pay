e924b6a8b9d9251f5a9bdb45b0a64134115c0a10
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-03-01 00:00:31 +0900

    Create firebase.ts: Firebase関連の処理

---
+++ src/firebase.ts
@@ -0,0 +1,69 @@
+//----- Firebase関連の処理 -----//
+
+import 'firebase/auth'
+
+import firebase from 'firebase/app'
+
+import { firebaseConfig } from './firebaseConfig'
+import { firebaseUserDataLoaded } from './main'
+import { usrAuthData } from './type'
+
+// ユーザーデータを保存
+let userData: usrAuthData = {
+  name: '',
+  uid: '',
+  mail: '',
+  google: '',
+  twitter: '',
+  facebook: '',
+  mailVerified: false,
+}
+
+// Firebaseを初期化
+firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth()
+
+// ログイン状態が変更されたときの処理
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // ログイン状態
+    if (location.pathname === '/login') {
+      location.pathname = '/'
+      return
+    }
+    console.log(user)
+    userData = {
+      name: user.displayName,
+      uid: user.uid,
+      mail: user.providerData.filter(
+        (provider) => provider?.providerId === 'password'
+      )[0]?.uid,
+      google: user.providerData.filter(
+        (provider) => provider?.providerId === 'google.com'
+      )[0]?.photoURL,
+      twitter: user.providerData.filter(
+        (provider) => provider?.providerId === 'twitter.com'
+      )[0]?.photoURL,
+      facebook: user.providerData.filter(
+        (provider) => provider?.providerId === 'facebook.com'
+      )[0]?.photoURL,
+      mailVerified: user.emailVerified,
+    }
+
+    console.log(userData)
+    firebaseUserDataLoaded()
+  } else {
+    // 非ログイン状態
+    if (location.pathname !== '/login') {
+      location.pathname = '/login'
+    }
+  }
+})
+
+// Firebaseの認証にデバイスの言語を利用する
+auth.languageCode = 'ja'
+
+// ログイン状態の保持期間の設定
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).catch(console.error)
+
+export { firebase, userData, auth }
